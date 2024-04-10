df466edf4e3d0051e7f702d9418e4407b5fd39f4
Werlyn Rodriguez <rdwerlynjose.16@gmail.com>
2024-04-10 11:14:03 -0600

    Refactor routes.rb to use "features" instead of "earthquake"

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
   namespace :api do
     namespace :v1 do
-      resources :earthquake, only: [:index] do
+      resources :features, only: [:index], controller: "earthquake" do
         resources :comment, only: [:create, :index]
       end
     end
